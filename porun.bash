fd2e3a1d14be3a70f009921465599f0b6e6e6508
Greg Sanders <ElCoronel@users.noreply.github.com>
2017-08-02 13:40:03 -0400

    Update porun.bash

--- porun.bash
+++ porun.bash
@@ -2,37 +2,37 @@
 
 CURDTE=`date +%y%m%d-%H%M`
 
-if [ -f /export/home/tcpo/ponotification ]
+source /export/home/user/env.list
+
+if [ -f /export/home/user/po/ponotification ]
         then
-        cd /export/home/tcpo/
-        /export/home/tcpo/ponotification /export/home/tcpo/configuration.xml
+        cd /export/home/user/po/
+        /export/home/user/po/ponotification /export/home/user/po/configuration.xml
 
-        if [ -f /export/home/tcpo/email/POEMailList.txt ]
+        if [ -f /export/home/user/po/email/POEMailList.txt ]
         then
 
-                cd /export/home/tcpo/email
+                cd /export/home/user/po/email
 
-                for i in `ls /export/home/tcpo/email/ | grep PO`
-                        do dos2unix /export/home/tcpo/email/$i /export/home/tcpo/email/$i
+                for i in `ls /export/home/user/po/email/ | grep PO`
+                        do dos2unix /export/home/user/po/email/$i /export/home/user/po/email/$i
                 done
 
-                INPUT=/export/home/tcpo/email/POEMailList.txt
+                INPUT=/export/home/user/po/email/POEMailList.txt
                 OLDIFS=$IFS
                 IFS=,
                 [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
                 while read addr1 addr2 sub mesg
                         do
-                                /bin/mailx -s $sub -r wralc.tila.centra@us.af.mil $addr1 $addr2 < /export/home/tcpo/$mesg
+                                /bin/mailx -s $sub -reply-to@addr.ess $addr1 $addr2 < /export/home/user/po/$mesg
                         done < $INPUT
                 IFS=$OLDIFS
-                mv /export/home/tcpo/email/POEMailList.txt /export/home/tcpo/email/archive/POEMailList.txt.$CURDTE      
-                /usr/bin/gzip /export/home/tcpo/email/archive/POEMailList.txt.$CURDTE
-                for y in `ls /export/home/tcpo/email | grep Message`
-                        do mv /export/home/tcpo/email/$y /export/home/tcpo/email/archive/$y.$CURDTE
-                        /usr/bin/gzip /export/home/tcpo/email/archive/$y.$CURDTE
+                mv /export/home/user/po/email/POEMailList.txt /export/home/user/po/email/archive/POEMailList.txt.$CURDTE
+                /usr/bin/gzip /export/home/user/po/email/archive/POEMailList.txt.$CURDTE
+                for y in `ls /export/home/user/po/email | grep Message`
+                        do mv /export/home/user/po/email/$y /export/home/user/po/email/archive/$y.$CURDTE
+                        /usr/bin/gzip /export/home/user/po/email/archive/$y.$CURDTE
                 done
         fi
 
 fi
-
-exit 0
