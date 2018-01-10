4b0c3f08434eda2fad41b6be4b361b4063cccc8b
Greg Sanders <ElCoronel@users.noreply.github.com>
2018-01-10 11:46:36 -0500

    Update nfscheck.sh
    updated to check if filesystem is mounted

--- nfscheck.sh
+++ nfscheck.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/bash
+### current date for tmp file name
 CURDATE=`date +%y%m%d-%H%MA`
-NFSMOUNT=( "/path/1" "/path/2" )
-
+### edit array to reflect nfs mount locations
+NFSMOUNT=( "/path/1" "/path/2" "/path/3" )
+### stat each location and verify mounted
 for i in "${NFSMOUNT[@]}"
     do
         if [[ -z $(timeout -k 15s 10s stat -t $i 2>&-) ]]; then
             echo "stat command for $i failed!" >> /tmp/nfs_fail_$CURDATE.txt
+        else
+            if [[ -z $(timeout -k 15s 10s df -h | awk '{print $6}' | grep -w $i 2>&-) ]]; then
+                echo "$i is not mounted!" >> /tmp/nfs_fail_$CURDATE.txt
+            fi
         fi
     done
-
+### check for nfs client service
 if [[ -f /tmp/nfs_fail_$CURDATE.txt ]]; then
     if [[ ! $(svcs /network/nfs/client | grep -v STATE | grep online) ]]; then
         echo "NFS Client service NOT running!" >> /tmp/nfs_fail_$CURDATE.txt
@@ -16,7 +22,7 @@ if [[ -f /tmp/nfs_fail_$CURDATE.txt ]]; then
         echo "NFS Client service is running." >> /tmp/nfs_fail_$CURDATE.txt
     fi
 fi
-
+### email if tmp file exists
 if [[ -f /tmp/nfs_fail_$CURDATE.txt ]]; then
     cat /tmp/nfs_fail_$CURDATE.txt | mailx -s "NFS Failure $(hostname) $(date '+%F %H:%M')" someone@some.addy
     rm -rf /tmp/nfs_fail_$CURDATE.txt
