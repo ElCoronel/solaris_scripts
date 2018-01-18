9ba4b2a0bb8a769fcb4be1643ab3de9c54d80e81
Greg Sanders <ElCoronel@users.noreply.github.com>
2018-01-18 10:39:40 -0500

    Update nfscheck.sh
    final version I hope

--- nfscheck.sh
+++ nfscheck.sh
@@ -1,51 +1,49 @@
 #!/usr/bin/bash
-### current date for tmp file name
+### current date for message file name
 CURDATE=`date +%y%m%d-%H%MA`
 ### edit array to reflect nfs mount locations
 NFSMOUNT=( "/mount/1" "/mount/2")
-### test to see if failure still exists
-if [[ -f /tmp/nfs_failed.txt ]]; then
-    for h in `sort /tmp/nfs_failed.txt | awk '{print $1}' | uniq`
-        do
-            if [[ -z $(timeout -k 15s 10s stat -t $h 2>&-) ]]; then
-                echo "$h $(date '+%F %H:%M') stat failed" >> /tmp/nfs_failed.txt
-            elif [[ -z $(mount | awk '{print $1}' | grep $h) ]]; then
-                echo "$h $(date'+%F %H:%M') mount not present" >> /tmp/nfs_failed.txt
+### checks nfs and mount, writes to message file if new failure, writes to log if old
+for a in "${NFSMOUNT[@]}"
+    do
+        NAME=`echo $a | sed 's/\//\./g'`
+        if [[ -f /tmp/nfs_fail$NAME.out ]]; then
+            timeout -k 15s 10s df -h &>/dev/null
+            if [[ -z $(timeout -k 15s 10s stat -t $a 2>&-) ]]; then
+                echo "$a $(date '+%F %H:%M') stat failed" >> /tmp/nfs_fail$NAME.out
+            elif [[ -z $(mount | awk '{print $1}' | grep $a) ]]; then
+                echo "$a $(date'+%F %H:%M') mount not present" >> /tmp/nfs_fail$NAME.out
             else
-                echo "$h is available again." | mailx -s "NFS Correction $(hostname) $(date '+%F %H:%M')" someone@somewhere
-                rm -rf /tmp/nfs_failed.txt
+                echo "$a is available again." | mailx -s "NFS Correction $(hostname) $(date '+%F %H:%M')" someone@somewhere
+                rm -rf /tmp/nfs_fail$NAME.out
             fi
-        done
-else
-### stat each location and verify mounted
-    for i in "${NFSMOUNT[@]}"
-        do
-            if [[ -z $(timeout -k 15s 10s stat -t $i 2>&-) ]]; then
-                sleep 5m
-                if [[ -z $(timeout -k 15s 10s stat -t $i 2>&-) ]]; then
-                    echo "stat command for $i failed!" >> /tmp/nfs_fail_$CURDATE.txt
-                    echo "$i $(date '+%F %H:%M') stat failed" >> /tmp/nfs_failed.txt
-                fi
-            else
-                if [[ -z $(mount | awk '{print $1}' | grep $i) ]]; then
-                    echo "$i is not mounted!" >> /tmp/nfs_fail_$CURDATE.txt
-                    echo "$i $(date '+%F %H:%M') mount not present" >> /tmp/nfs_failed.txt
-                fi
-            fi
-        done
-### check for nfs client service
-    if [[ -f /tmp/nfs_fail_$CURDATE.txt ]]; then
-        if [[ -z $(svcs /network/nfs/client | grep -v STATE | grep online) ]]; then
-            echo "NFS Client service NOT running!" >> /tmp/nfs_fail_$CURDATE.txt
         else
-            echo "NFS Client service is running." >> /tmp/nfs_fail_$CURDATE.txt
+            if [[ -z $(timeout -k 15s 10s stat -t $a 2>&-) ]]; then
+                sleep 3m
+                if [[ -z $(timeout -k 15s 10s stat -t $a 2>&-) ]]; then
+                    echo "stat command for $a failed!" >> /tmp/nfs_message_$CURDATE.txt
+                    echo "$a $(date '+%F %H:%M') stat failed" >> /tmp/nfs_fail$NAME.out
+                fi
+            else
+                if [[ -z $(mount | awk '{print $1}' | grep $a) ]]; then
+                    echo "$a is not mounted!" >> /tmp/nfs_message_$CURDATE.txt
+                    echo "$a $(date '+%F %H:%M') mount not present" >> /tmp/nfs_fail$NAME.out
+                fi
+            fi
         fi
-    fi
-### email if tmp file exists
-    if [[ -f /tmp/nfs_fail_$CURDATE.txt ]]; then
-        cat /tmp/nfs_fail_$CURDATE.txt | mailx -s "NFS Failure $(hostname) $(date '+%F %H:%M')" someone@somewhere
-        rm -rf /tmp/nfs_fail_$CURDATE.txt
+    done
+### check for nfs client service
+if [[ -f /tmp/nfs_message_$CURDATE.txt ]]; then
+    if [[ -z $(svcs /network/nfs/client | grep -v STATE | grep online) ]]; then
+        echo "NFS Client service NOT running!" >> /tmp/nfs_message_$CURDATE.txt
     else
-        exit
+        echo "NFS Client service is running." >> /tmp/nfs_message_$CURDATE.txt
     fi
+fi
+### email if tmp file exists
+if [[ -f /tmp/nfs_message_$CURDATE.txt ]]; then
+    cat /tmp/nfs_message_$CURDATE.txt | mailx -s "NFS Failure $(hostname) $(date '+%F %H:%M')" someone@somewhere
+    rm -rf /tmp/nfs_message_$CURDATE.txt
+else
+    exit
 fi
