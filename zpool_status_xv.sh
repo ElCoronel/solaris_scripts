de67aa75496806682b4fbb4311cf934936b3b9f7
Greg Sanders <ElCoronel@users.noreply.github.com>
2018-01-30 15:09:51 -0500

    Update zpool_status_xv.sh

--- zpool_status_xv.sh
+++ zpool_status_xv.sh
@@ -7,14 +7,14 @@ for i in `cat $HOSTLIST | awk '{print $1}'`
         do
                 REMHOST=`cat $HOSTLIST | grep $i | awk '{print $2}'`
                 if [[ -z $(ssh user@$i /usr/sbin/zpool status -xv | grep "all pools are healthy") ]]; then
-### if error exists, loop through errored pool
+### if error exists, loop through errored pools
                         ZPOOL=(`ssh user@$i /usr/sbin/zpool status -xv | /usr/gnu/bin/grep 'pool:' | awk '{print $2}'`)
                         for a in "${ZPOOL[@]}"
                                 do
-### if error file exists log dated error
+### if error file exists log error
                                 if [[ -f $LOGDIR/$REMHOST/$a-error.out ]]; then
                                         echo "$i $REMHOST - `date` - `ssh user@$i /usr/sbin/zpool status -xv | grep $a | awk '{print $2}' | grep $a`" >> $LOGDIR/$REMHOST/zpoolxv.$(date +%Y-%m).log
-### if error file does not exist, create, log date error and send email
+### if error file does not exist, create, log error and send email
                                 else
                                         if [ ! -d $LOGDIR/$REMHOST ]; then
                                         mkdir $LOGDIR/$REMHOST
@@ -25,7 +25,7 @@ for i in `cat $HOSTLIST | awk '{print $1}'`
                                         cat $LOGDIR/$REMHOST/$a-error.out | mailx -s "zpool status error $REMHOST $(date '+%F %H:%M')" someone@somebody
                                 fi
                         done
-### if error condition does not exist log dated healthy status
+### if error condition does not exist log healthy status
                 else
                         if [ ! -d $LOGDIR/$REMHOST ]; then
                                 mkdir $LOGDIR/$REMHOST
