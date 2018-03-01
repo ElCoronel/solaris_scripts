8ca03fb8dd843a35f98517e7a8522f26100db531
Greg Sanders <ElCoronel@users.noreply.github.com>
2018-03-01 10:44:44 -0500

    Update zpool_status_rotate.sh

--- zpool_status_rotate.sh
+++ zpool_status_rotate.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/bash
-### deletes year old log files, moves current logs to archive, runs once a month
+### deletes year old log files, moves current logs to archive, deletes error files, runs once a month
 LOGDIR=$HOME/logs
 find $LOGDIR/*/zpool_status -type f -ctime +365 -exec rm {} \;
-for l in `find $LOGDIR/*/zpool_status -depth -type f -print | grep zpool_status`
-        do
-                mv $l $(date +%y-%m-%d).archive.$l
-        done
+for l in `find $LOGDIR/*/zpool_status -depth -type f -print | grep zpoolxv`; do
+        mv $l $l.$(date +%y-%m-%d).archive
+done
+for d in `find $LOGDIR/*/zpool_status -depth -type f -print | grep error`; do
+        rm -rf $d
+done
