d4c45885e7a594f6d0b60d95a6ea7a5b73387c1c
Greg Sanders <ElCoronel@users.noreply.github.com>
2018-03-30 13:01:59 -0400

    Update file_cleanup_finder.sh

--- file_cleanup_finder.sh
+++ file_cleanup_finder.sh
@@ -1,22 +1,39 @@
 #! /usr/bin/bash
 ## finds 10 largest files in filesystem provided as arguments
-## usage file_cleanup_finder.sh <some filesystem> <some filesystem> ...
+## usage file_cleanup_finder.sh <save|silent> <some filesystem> <some filesystem> ...
+## if save or silent is not provided it assumed just screen output and no file save
 IFS=$'\n'
+RDATE=`date '+%m%d%Y-%H%M-%s'`
 if [ $# -eq 0 ]; then
-    echo "Usage: ./file_cleanup_funder.sh <some filesystem> <some filesystem> ..."
+    echo "Usage: ./file_cleanup_funder.sh <save|silent> <some filesystem> <some filesystem> ..."
     exit 1
 fi
 ZARRAY=( "$@" )
-for f in "${ZARRAY[@]}"; do
-    /usr/gnu/bin/find $f -type f -print0 | /usr/gnu/bin/xargs -0 /usr/gnu/bin/du | sort -n | tail -10 | cut -f2 >> /tmp/file_cleanup_finder.list
-done
+if [[ $1 =~ ^(save|silent)$ ]]; then
+    for f in "${ZARRAY[@]:1}"; do
+        /usr/gnu/bin/find $f -type f -print0 | /usr/gnu/bin/xargs -0 /usr/gnu/bin/du | sort -n | tail -10 | cut -f2 >> /tmp/file_cleanup_finder.list
+    done
+else
+    for f in "${ZARRAY[@]}"; do
+        /usr/gnu/bin/find $f -type f -print0 | /usr/gnu/bin/xargs -0 /usr/gnu/bin/du | sort -n | tail -10 | cut -f2 >> /tmp/file_cleanup_finder.list
+    done
+fi
 for g in `cat /tmp/file_cleanup_finder.list`; do
     DUSH=$(/usr/gnu/bin/du -sh $g)
     ## truncate the filename output if needed
     #LUSH=$(echo ${DUSH:0:45})
     LSLA=$(ls -la $g | awk '{print $6, $7, $8}')
-    printf "%-15s%s\n" "$LSLA" "$DUSH" >> /tmp/file_cleanup_finder.out
+    printf "%-15s%s\n" "$LSLA" "$DUSH" >> /tmp/file_cleanup_finder-$RDATE.out
 done
-cat /tmp/file_cleanup_finder.out
-rm -rf /tmp/file_cleanup_finder.out
-rm -rf /tmp/file_cleanup_finder.list
+if [[ $1 == "save" ]]; then
+    cat /tmp/file_cleanup_finder-$RDATE.out
+    echo "Output saved to /tmp/file_cleanup_finder-$RDATE.out"
+    rm -rf /tmp/file_cleanup_finder.list
+elif [[ $1 == "silent" ]]; then
+    echo "Output saved to /tmp/file_cleanup_finder-$RDATE.out"
+    rm -rf /tmp/file_cleanup_finder.list
+else
+    cat /tmp/file_cleanup_finder-$RDATE.out
+    rm -rf /tmp/file_cleanup_finder.list
+    rm -rf /tmp/file_cleanup_finder-$RDATE.out
+fi
