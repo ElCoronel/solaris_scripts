5cc0ee39bb19c49630ece2879bd9e29bc3a683b3
Greg Sanders <ElCoronel@users.noreply.github.com>
2018-05-15 10:52:04 -0400

    Update nfscheck.sh
    added extra timeout to touch

--- nfscheck.sh
+++ nfscheck.sh
@@ -2,33 +2,39 @@
 ### current date for message file name
 CURDATE=`date +%y%m%d-%H%MA`
 ### edit array to reflect nfs mount locations
-NFSMOUNT=( "/mount/1" "/mount/2")
+NFSMOUNT=( "/eopf-qacfs01/qa_dsks/qa_edms" )
 ### checks nfs and mount, writes to message file if new failure, writes to log if old
 for a in "${NFSMOUNT[@]}"
     do
         NAME=`echo $a | sed 's/\//\./g'`
         if [[ -f /tmp/nfs_fail$NAME.out ]]; then
             timeout -k 15s 10s df -h &>/dev/null
-            if [[ -z $(timeout -k 15s 10s stat -t $a 2>&-) ]]; then
+            if ! $(timeout -k 15s 10s stat -t $a > /dev/null 2>&1); then
                 echo "$a $(date '+%F %H:%M') stat failed" >> /tmp/nfs_fail$NAME.out
+            elif ! $(timeout -k 15s 10s su - tehriapp -c "touch $a/nfs_2b.test" > /dev/null 2>&1); then
+                            echo "$a $(date '+%F %H:%M') touch failed" >> /tmp/nfs_fail$NAME.out
             elif [[ -z $(mount | awk '{print $1}' | grep $a) ]]; then
                 echo "$a $(date'+%F %H:%M') mount not present" >> /tmp/nfs_fail$NAME.out
             else
-                echo "$a is available again." | mailx -s "NFS Correction $(hostname) $(date '+%F %H:%M')" someone@somewhere
+                echo "$a is available again." | mailx -s "ALERT CLEARED: NFS Correction $(hostname) $(date '+%F %H:%M')" hpux_admin@opm.gov
                 rm -rf /tmp/nfs_fail$NAME.out
             fi
         else
-            if [[ -z $(timeout -k 15s 10s stat -t $a 2>&-) ]]; then
+            if ! $(timeout -k 15s 10s stat -t $a > /dev/null 2>&1); then
                 sleep 3m
-                if [[ -z $(timeout -k 15s 10s stat -t $a 2>&-) ]]; then
+                if ! $(timeout -k 15s 10s stat -t $a > /dev/null 2>&1); then
                     echo "stat command for $a failed!" >> /tmp/nfs_message_$CURDATE.txt
                     echo "$a $(date '+%F %H:%M') stat failed" >> /tmp/nfs_fail$NAME.out
                 fi
-            else
-                if [[ -z $(mount | awk '{print $1}' | grep $a) ]]; then
+            elif ! $(timeout -k 15s 10s su - tehriapp -c "touch $a/nfs_2b.test" > /dev/null 2>&1); then
+                sleep 3m
+                if ! $(timeout -k 15s 10s su - tehriapp -c "touch $a/nfs_2b.test" > /dev/null 2>&1); then
+                    echo "touch command for $a failed!" >> /tmp/nfs_message_$CURDATE.txt
+                    echo "$a $(date '+%F %H:%M') touch failed" >> /tmp/nfs_fail$NAME.out
+                fi
+            elif [[ -z $(mount | awk '{print $1}' | grep $a) ]]; then
                     echo "$a is not mounted!" >> /tmp/nfs_message_$CURDATE.txt
                     echo "$a $(date '+%F %H:%M') mount not present" >> /tmp/nfs_fail$NAME.out
-                fi
             fi
         fi
     done
@@ -42,7 +48,7 @@ if [[ -f /tmp/nfs_message_$CURDATE.txt ]]; then
 fi
 ### email if tmp file exists
 if [[ -f /tmp/nfs_message_$CURDATE.txt ]]; then
-    cat /tmp/nfs_message_$CURDATE.txt | mailx -s "NFS Failure $(hostname) $(date '+%F %H:%M')" someone@somewhere
+    cat /tmp/nfs_message_$CURDATE.txt | mailx -s "NEW ALERT: NFS Failure $(hostname) $(date '+%F %H:%M')" hpux_admin@opm.gov
     rm -rf /tmp/nfs_message_$CURDATE.txt
 else
     exit
